978122a9bc40261e8d4ca43918751a29c48aa0e5
Sahad16 <sahadjos13@gmail.com>
2021-06-03 19:33:19 -0500

    cambios

--- 2_examenc2/array.c
+++ 2_examenc2/array.c
@@ -3,12 +3,13 @@
 int main(){
 	
 	char arrayChar[5];
-	char i = 0;
+	int i=0;
 
-	for(char i= 'a'; i< 'f'; i++){
-		arrayChar[i - a] = i;
+	for (int letra = 97; letra <102; letra++){	
+		arrayChar[i] = letra;
 		i++;
 	}
+	
 
 	char *punteroChar = arrayChar;
 
@@ -16,19 +17,19 @@ int main(){
        	punteroCharDos = &arrayChar[0];
 
 
-    printf("\nContenido primer elemento de arrayChar = %c", arrayChar[0]);
+    printf("\nContenido  elemento 1 de arrayChar = %c", arrayChar[0]);
 
-    printf("\nContenido primer elemento de punteroChar = %c", *punteroChar);
+    printf("\nContenido  elemento 1 de punteroChar = %c", *punteroChar);
 
-    printf("\nContenido primer elemento de punteroCharDos = %c", *punteroCharDos);
+    printf("\nContenido  elemento 1 de punteroCharDos = %c", *punteroCharDos);
 
-    printf("\nContenido quinto elemento de arrayChar = %c", arrayChar[4]);
+    printf("\nContenido  elemento 1 de arrayChar = %c", arrayChar[4]);
 	
 	//aritmetica de punteros
 
-    printf("\nContenido quinto elemento de punteroChar con aritmetica de punteros = %c", *(punteroChar + 4)); 
+    printf("\n 5to elemento punteroChar con aritmetica de punteros = %c", *(punteroChar + 4)); 
 
-    printf("\nContenido quinto elemento de punteroCharDos con arimética de punteros = %c", *(punteroCharDos + 4));
+    printf("\n 5to elemento punteroCharDos con arimetica de punteros = %c", *(punteroCharDos + 4));
 
       for (i = 0; i < 5; i++){
         printf("\nContenido [%i] de punteroCharDos = %c", i, *(punteroCharDos + i));
--- 2_examenc2/puntero.c
+++ 2_examenc2/puntero.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+int main (){
 char x,y  ;
 char *p;/*puntero*/
 char *puntero;
 
 
- p = $x;
- puntero= $y;
+ p = &x;
+ puntero= &y;
 
-x='x';
+	x='x';
 
 
 printf("la direccion de memoria de 'x' es: %p \n", &x);
-	printf("y su contenido es : %c \n", *p);
+	printf("el contenido del puntero p es : %c \n", *p);
 
 *p=(*p)+1;
 
-printf("y su contenido es : %c \n", *p);
+printf("el  contenido del puntero p es : %c \n", *p);
 
 *p=(*p)+2;
-printf("y su contenido es : %c \n", *p);
+printf("el  contenido del puntero p es  : %c \n", *p);
 	
 	
 y='y';
 printf("la direccion de memoria de 'y' es: %p \n", &y);
-	printf("y su contenido es : %c \n", *puntero);
+	printf("y su contenido del puntero puntero : %c \n", *puntero);
 	
+	return 0;
+}
